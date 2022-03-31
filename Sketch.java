21f2fea3b30e7fc1ac5d0c23945d1d15c85d9c12
Keira Hosey <78433426+keira-h@users.noreply.github.com>
2022-03-31 12:55:17 +0000

    section 4 done

--- Sketch.java
+++ Sketch.java
@@ -146,7 +146,26 @@ public class Sketch extends PApplet {
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4(){
+    int intX = 0;
+    int intY = 0;
 
+    for(int intRow = 0; intRow < 30; intRow++){
+      for(int intColumn = 0; intColumn < 30; intColumn++){
+        intX = 903 + (intRow * 10); 
+        intY = 300 + 3 + (intColumn * 10);
+        
+        if (intColumn % 2 == 0 && intRow % 2 == 0) {
+          fill(255);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        else {
+          fill(0);
+          noStroke();
+          rect(intX, intY, 5, 5);
+        }
+        }
+      }
   }
 
   /**
